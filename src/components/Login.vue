a8e2d463b970d6c23e2531456b9bb98b8ce82159
Danling Wu <danling.wu.927@gmail.com>
2020-08-02 10:33:32 +0100

    02/08 login works

---
+++ src/components/Login.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <form name="form" @submit.prevent="handleLogin(user)">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input
+              v-model="user.username"
+              type="text"
+              class="form-control border"
+              name="username"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              v-model="user.password"
+              type="text"
+              class="form-control border"
+              name="password"
+              required
+            />
+          </div>
+          <div class="form-group">
+                        <input
+              v-model="user.token"
+              type="hidden"
+            />
+            <button type="submit" class="btn btn-primary btn-block" :disabled="loading">
+              <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+              <span>Login</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      user: {},
+      loading: false,
+    }
+  },
+  computed:{
+    ...mapState({
+        //user: state => state.auth.user,
+        token: state => state.auth.token
+    }),
+   },
+  methods:{
+    async handleLogin(user) {
+      console.log(user)
+      await this.$store.dispatch('auth/handleLogin', user)
+      //console.log("my token is: " + localStorage.getItem('user.token'));
+      console.log(this.token);
+
+      this.$router.push('/')
+      //await this.$store.dispatch('admin/login', post)
+      //this.$router.push('/Admin')
+    },
+  },
+
+}
+</script>
+
+<!--
+import axios from 'axios'
+
+const API_URL_Auth = 'http://127.0.0.1:5000/auth'
+
+  methods:{
+    async handleLogin(user) {
+      console.log(user)
+      let res = await axios.post(`${API_URL_Auth}/auth/login`, user)
+      console.log(res.data.token)
+      localStorage.setItem('user', JSON.stringify(res.data));
+      console.log("my token is: " + localStorage.getItem('userToken'));
+
+      this.$router.push('/')
+      //await this.$store.dispatch('admin/login', post)
+      //this.$router.push('/Admin')
+    },
+  },
+
+     beforeMount() {
+      console.log("my token is: " + user);
+      this.$store.dispatch('auth/loadAdminUser')
+      console.log("my token is: " + user);
+    },
+        ...mapActions('auth', {
+      handleLogin: 'handleLogin'
+    }),
+  -->
